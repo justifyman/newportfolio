8c1e4d9895108bb5e26561317d157e242bead491
justify <143286685+justifyman@users.noreply.github.com>
2025-07-15 10:43:46 +0400

    Updated LoadingSequence.tsx

--- src/components/LoadingSequence.tsx
+++ src/components/LoadingSequence.tsx
@@ -20,12 +20,11 @@ const PageTransition: FC<PageTransitionProps> = ({ children }) => {
 
   return (
     <>
-      {/* Actual page content: always rendered */}
+      {/* Render actual site content here */}
       <div className="relative z-0">{children}</div>
 
-      {/* Transition overlays */}
+      {/* Overlays on top */}
       <div className="fixed inset-0 z-[9999] pointer-events-none overflow-hidden">
-        {/* Black overlay slides up */}
         <div
           className={`absolute inset-0 bg-black transition-transform duration-500 ease-in-out ${
             showBlack ? "translate-y-0" : "-translate-y-full"
@@ -33,9 +32,8 @@ const PageTransition: FC<PageTransitionProps> = ({ children }) => {
           style={{ zIndex: 30 }}
         />
 
-        {/* Blue overlay slides up after black */}
         <div
-          className={`absolute inset-0 bg-blue-950 transition-transform duration-500 ease-in-out ${
+          className={`absolute inset-0 bg-blue-900 transition-transform duration-500 ease-in-out ${
             showBlue ? "translate-y-0" : "-translate-y-full"
           }`}
           style={{ zIndex: 20 }}
