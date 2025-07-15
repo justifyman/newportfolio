93a91ba94cd9a60f31fc3fdfa4b9e9a14020f4ac
justify <143286685+justifyman@users.noreply.github.com>
2025-07-15 10:52:32 +0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import PageTransition from './components/LoadingSequence';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Work from './pages/Work';
