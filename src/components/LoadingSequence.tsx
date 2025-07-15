1f95afd874183cee47700814e6c5567e4f60ca4d
justify <143286685+justifyman@users.noreply.github.com>
2025-07-15 10:49:02 +0400

    Updated LoadingSequence.tsx

--- src/components/LoadingSequence.tsx
+++ src/components/LoadingSequence.tsx
@@ -7,41 +7,27 @@ interface PageTransitionProps {
 
 const PageTransition: FC<PageTransitionProps> = ({ children, onComplete }) => {
   const [showBlack, setShowBlack] = useState(true);
-  const [blueSlidUp, setBlueSlidUp] = useState(false);
 
   useEffect(() => {
-    const timer1 = setTimeout(() => setShowBlack(false), 500);
-
-    const timer2 = setTimeout(() => {
-      setBlueSlidUp(true);
+    const timer = setTimeout(() => {
+      setShowBlack(false);
       if (onComplete) onComplete();
-    }, 1000);
+    }, 500);
 
-    return () => {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
-    };
+    return () => clearTimeout(timer);
   }, [onComplete]);
 
   return (
     <>
       {children && <div className="relative z-0">{children}</div>}
 
+      {/* Black overlay that slides up */}
       <div className="fixed inset-0 z-[9999] pointer-events-none overflow-hidden">
-        {/* Black slides up */}
         <div
           className={`absolute inset-0 bg-black transition-transform duration-500 ease-in-out ${
             showBlack ? "translate-y-0" : "-translate-y-full"
           }`}
           style={{ zIndex: 30 }}
-        />
-
-        {/* Blue slides up AFTER black */}
-        <div
-          className={`absolute inset-0 bg-blue-900 transition-transform duration-500 ease-in-out ${
-            blueSlidUp ? "-translate-y-full" : "translate-y-0"
-          }`}
-          style={{ zIndex: 20 }}
         />
       </div>
     </>
